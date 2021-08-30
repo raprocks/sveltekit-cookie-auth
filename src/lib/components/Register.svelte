06898ee926746e11908ad40948d3a4da6117bb65
Rohit Patil <rahulhimesh09@gmail.com>
2021-08-30 22:36:19 +0530

    [ADD] Register Component

---
+++ src/lib/components/Register.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	let email;
+	let name;
+	let password;
+
+	const dispatch = createEventDispatcher();
+
+	async function login() {
+		dispatch('success');
+	}
+</script>
+
+<h2>Register</h2>
+<input type="email" bind:value={email} placeholder="email" />
+<input type="name" placeholder="name" bind:value={name} />
+<br />
+<input type="password" placeholder="password" bind:value={password} />
+<button on:click={login}>Login to Profile</button>
