f0944680ba61003e7a55cff8ddda96244a4cae89
Rohit Patil <rahulhimesh09@gmail.com>
2021-08-30 22:47:05 +0530

    [UPDATE] Implemented login function
    dispatches success event if login succeeds else shows an error

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -1,12 +1,33 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
-	let email;
-	let password;
+	let email: string;
+	let password: string;
+	let error: string;
 
 	const dispatch = createEventDispatcher();
 
 	async function login() {
-		dispatch('success');
+		error = undefined;
+		try {
+			const res = await fetch('/auth/login', {
+				method: 'POST',
+				body: JSON.stringify({
+					email,
+					password
+				}),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			if (res.ok) {
+				dispatch('success');
+			} else {
+				error = 'An error occurred while Logging In';
+			}
+		} catch (e) {
+			console.log(`error occured while Login`, e);
+			error = 'An error occurred while Logging In';
+		}
 	}
 </script>
 
@@ -14,6 +35,7 @@
 <input bind:value={email} type="email" placeholder="email" />
 <br />
 <input type="password" bind:value={password} placeholder="password" />
+{#if error}{error}{/if}
 <button on:click={login}>Login to Profile</button>
 
 <style>
