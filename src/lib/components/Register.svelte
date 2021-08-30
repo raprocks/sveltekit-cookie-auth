2fcd5e26bd03617debb68cb0b2c4a8f87a993006
Rohit Patil <rahulhimesh09@gmail.com>
2021-08-30 22:48:09 +0530

    [UPDATE] Implemeted register function

--- src/lib/components/Register.svelte
+++ src/lib/components/Register.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
-	let email;
-	let name;
-	let password;
-
+	let email: string;
+	let name: string;
+	let password: string;
+	let error: string;
 	const dispatch = createEventDispatcher();
 
-	async function login() {
-		dispatch('success');
+	async function register() {
+		error = undefined;
+		try {
+			const res = await fetch('/auth/register', {
+				method: 'POST',
+				body: JSON.stringify({
+					email,
+					password,
+					name
+				}),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			if (res.ok) {
+				dispatch('success');
+			} else {
+				error = 'An error occurred while Registering';
+			}
+		} catch (e) {
+			console.log(`error occured while registering`, e);
+			error = 'An error occurred while Registering';
+		}
 	}
 </script>
 
@@ -16,4 +37,5 @@
 <input type="name" placeholder="name" bind:value={name} />
 <br />
 <input type="password" placeholder="password" bind:value={password} />
-<button on:click={login}>Login to Profile</button>
+{#if error}{error}{/if}
+<button on:click={register}>Login to Profile</button>
