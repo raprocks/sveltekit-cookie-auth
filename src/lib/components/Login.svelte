e69e6b4f87fd89fb489dca89673bc312f967d3e4
Rohit Patil <rahulhimesh09@gmail.com>
2021-08-30 22:36:10 +0530

    [ADD] Login Component

---
+++ src/lib/components/Login.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	let email;
+	let password;
+
+	const dispatch = createEventDispatcher();
+
+	async function login() {
+		dispatch('success');
+	}
+</script>
+
+<h2>Login</h2>
+<input bind:value={email} type="email" placeholder="email" />
+<br />
+<input type="password" bind:value={password} placeholder="password" />
+<button on:click={login}>Login to Profile</button>
+
+<style>
+</style>
