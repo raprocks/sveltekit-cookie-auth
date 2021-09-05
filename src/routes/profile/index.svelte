a0f46089b4abe3f1f27f2153742dcf7f9b82b809
Rohit Patil <rahulhimesh09@gmail.com>
2021-09-05 15:01:53 +0530

    [UPDATE] Profile page
    added the load function in module script to check weather user is
    authenticated or not and render page with profile data if they are.
    else redirect to unauthorised page.

--- src/routes/profile/index.svelte
+++ src/routes/profile/index.svelte
@@ -1,1 +1,28 @@
+<script context="module" lang="ts">
+	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
+	export async function load({ session }: LoadInput): Promise<LoadOutput> {
+		if (!session.authenticated) {
+			return {
+				status: 302,
+				redirect: '/auth/unauthorized'
+			};
+		}
+		const res = await fetch('/user');
+		const user = await res.json();
+		return {
+			props: {
+				email: session.email,
+				user
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let email: any;
+	export let user: { name: any };
+</script>
+
 <h1>Profile</h1>
+{email} <br />
+{user.name}
