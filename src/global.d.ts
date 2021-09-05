59772559e7411ced765f58b31864d2c9733ff750
Rohit Patil <rahulhimesh09@gmail.com>
2021-09-05 14:53:47 +0530

    [ADD] intefarce for environment variables

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="@sveltejs/kit" />
+
+interface ImportMetaEnv {
+	VITE_REDIS_PORT: string;
+	VITE_REDIS_URL: string;
+	VITE_REDIS_PASSWORD: string;
+	VITE_REDIS_API_AUTH_KEY: string;
+}
