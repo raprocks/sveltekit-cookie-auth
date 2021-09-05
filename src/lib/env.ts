31cc45e44b107b22a496ddc7cc4766b98a6497cd
Rohit Patil <rahulhimesh09@gmail.com>
2021-09-05 14:55:02 +0530

    [ADD] Entry point for environment variables
    each of the variables here are a proxy to the environemnt variables from
    .env file

---
+++ src/lib/env.ts
@@ -0,0 +1,4 @@
+export const REDIS_API_AUTH_KEY = import.meta.env.VITE_REDIS_API_AUTH_KEY;
+export const REDIS_PASSWORD = import.meta.env.VITE_REDIS_PASSWORD;
+export const REDIS_URL = import.meta.env.VITE_REDIS_URL;
+export const REDIS_PORT = parseInt(import.meta.env.VITE_REDIS_PORT);
