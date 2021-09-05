d1b7d929f499327be66c6b087f4c062d72af480a
Rohit Patil <rahulhimesh09@gmail.com>
2021-09-05 15:50:37 +0530

    [ADD] Hooks
    handle the incoming request and add an authenticated mark for
    authenticated users  after checking the session id from the database
    populates headers if the session id matches else resolves the request
    with an unauthenticated mark.
    
    getSession updates session for the user with their authenticated status

---
+++ src/hooks.ts
@@ -0,0 +1,57 @@
+import * as cookie from 'cookie';
+import { Tedis } from 'tedis';
+import * as env from '$lib/env';
+import type { Handle } from '@sveltejs/kit';
+import type { GetSession } from '@sveltejs/kit/types/hooks';
+
+const db = new Tedis({
+	host: env.REDIS_URL,
+	password: env.REDIS_PASSWORD,
+	port: env.REDIS_PORT
+});
+
+export const handle: Handle = async function ({ request, resolve }) {
+	const cookies = cookie.parse(request.headers.cookie || '');
+
+	if (!cookies.session_id) {
+		request.locals.authenticated = false;
+		const resp = await resolve(request);
+		return {
+			...resp,
+			headers: {
+				...resp.headers
+			}
+		};
+	}
+	const userSession = JSON.parse((await db.get(cookies.session_id)).toString());
+	if (userSession) {
+		request.locals = {
+			...request.locals,
+			authenticated: true,
+			email: userSession.email
+		};
+	} else {
+		request.locals.authenticated = false;
+	}
+	const resp = await resolve(request);
+	return {
+		status: resp.status,
+		headers: {
+			...resp.headers
+		},
+		body: resp.body
+	};
+};
+
+export const getSession: GetSession = (request) => {
+	if (!request.locals.authenticated) {
+		return {
+			...request.locals,
+			authenticated: request.locals.authenticated
+		};
+	}
+	return {
+		authenticated: request.locals.authenticated,
+		email: request.locals.email
+	};
+};
